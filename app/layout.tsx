a370b91269ae9662192a7c545d4984250a7efcf7
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-11 20:02:51 +0000

    add react-hook-form, handle form submission

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body>
+    <html lang="en">
+      <body className={inter.variable}>
         <Theme appearance="light" accentColor="purple">
           <NavBar />
           <main className="p-5">{children}</main>
