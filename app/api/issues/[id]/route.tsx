e522d19350d2f3ca23e03a8f16f442bb944243a7
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-12 13:22:52 +0000

    delete an issue

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,7 +1,6 @@
 import { issueSchema } from "@/app/validationSchemas";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { error } from "console";
 
 export async function PATCH(
   request: NextRequest,
