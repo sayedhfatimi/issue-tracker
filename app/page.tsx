5f28fc907e77ebc3a392716a0cb41c3e2e28092f
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-12 19:51:02 +0000

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import Pagination from "./components/Pagination";
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <>
+      <div>Hello World</div>
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
+    </>
+  );
 }
