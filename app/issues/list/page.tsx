8d96af290e8474827d0e0b836e23678b7be0dcae
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-12 18:54:20 +0000

    sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -23,8 +23,15 @@ const IssuesPage = async ({
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
 
   const session = await getServerSession(authOptions);
