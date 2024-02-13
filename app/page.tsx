8b7f9a5c17f13d7172e8ff36df098d70d9e3be27
Sayed Hamid Fatimi <hamid@fatimi.co.uk>
2024-02-13 10:41:25 +0000

    build IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const openIssues = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgressIssues = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closedIssues = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
   return (
     <>
       <LatestIssues />
+      <IssueSummary
+        open={openIssues}
+        inProgress={inProgressIssues}
+        closed={closedIssues}
+      />
     </>
   );
 }
